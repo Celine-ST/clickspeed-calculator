0192ebfa4986a44b62ce34b6615f82196294f7db
Celine-ST <celine.quek10@rafflesgirlssch.edu.sg>
2023-07-08 09:44:23 +0800

    Initial Commit

---
+++ clickspeed calculator/clickspeed_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  clickspeed_calculatorApp.swift
+//  clickspeed calculator
+//
+//  Created by Celine Quek on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct clickspeed_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
